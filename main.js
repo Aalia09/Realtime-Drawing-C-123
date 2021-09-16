f25eb45e4e3a4bd7e536e3bd13d2e1c58ea5b047
Aalia09 <76902204+Aalia09@users.noreply.github.com>
2021-09-16 15:37:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftwristX= 0;
+rightwristX = 0;
+
 function setup()
 {
     video= createCapture(VIDEO);
     video.size(550 , 500);
 
-    canvas = createCanvas(550 ,550);
-    canvas.position(560 , 150);
+    canvas = createCanvas(550 ,540);
+    canvas.position(560 , 110);
 
     poseNet = ml5.poseNet(video , modelLoaded);
     poseNet.on("pose" , gotPoses);
@@ -20,9 +26,26 @@ function modelLoaded()
 function draw()
 {
     background("#6e75d4");
+    document.getElementById("square_side").innerHTML = "Width and Height of a Square will be = " + difference + "px";
+    fill("black");
+    stroke("peach");
+    square(noseX , noseY , difference);
 }
 
-function gotPoses()
+function gotPoses(results)
 {
-    console.log("Got poses");
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+        difference = floor(leftwristX - rightwristX);
+
+        console.log("leftwristX = " + leftwristX + "rightwristX = " + rightwristX + "difference = " + difference);
+    }
+
 }
